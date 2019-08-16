f493f4e1f9362ad7126d1b6d33db1436cb2464b7
gabriele <gabriele.valvano@gmail.com>
2019-08-16 10:23:36 +0100

    add generalized dice and remove unused metrics

--- idas/metrics/tf_metrics.py
+++ idas/metrics/tf_metrics.py
@@ -55,10 +55,11 @@ def dice_coe(output, target, axis=(1, 2, 3), smooth=1e-12):
     return score
 
 
-def jaccard_coe(output, target, axis=(1, 2, 3), smooth=1e-12, _name='jaccard_coe'):
-    """Soft Jaccard (also known as Intersection over Union) coefficient for comparing the similarity
+def generalized_dice_coe(output, target, axis=(1, 2, 3), smooth=1e-12):
+    """Generalized Soft Sørensen–Dice coefficient for comparing the similarity
     of two batch of data, usually be used for binary image segmentation
     i.e. labels are binary. The coefficient between 0 to 1, 1 means totally match.
+    This generalization of the Dice score weights each class over the number of pixels inside.
 
     Parameters
     -----------
@@ -67,38 +68,32 @@ def jaccard_coe(output, target, axis=(1, 2, 3), smooth=1e-12, _name='jaccard_coe
     target : Tensor
         The target distribution, format the same with `output`.
     axis : tuple of int
-        All dimensions are reduced, default ``[1,2,3]``.
+        All dimensions are reduced, default ``[1,2,3]`` for compatibility with dice_coe(). The actual reduction is on
+        all dimensions but the last one.
     smooth : float
         This small value will be added to the numerator and denominator.
-
-    _name : str
-        Name scope for the output value (internal usage only)
 
     Examples
     ---------
     '>>> outputs = tl.act.pixel_wise_softmax(network.outputs)'
-    '>>> jaccard_loss = 1 - jaccard_coe(outputs, y_)'
-
-    References
-    -----------
-    - `Wiki-Jaccard <https://en.wikipedia.org/wiki/Jaccard_index>`__
+    '>>> dice_loss = 1 - generalized_dice_coe(outputs, y_)'
 
     """
 
     assert output.dtype in [tf.float32, tf.float64]
     assert target.dtype in [tf.float32, tf.float64]
 
-    intersection = tf.reduce_sum(output * target, axis=axis)
+    intersection = tf.reduce_sum(output * target, axis=axis[:-1])
+    a = tf.reduce_sum(output, axis=axis[:-1])
+    b = tf.reduce_sum(target, axis=axis[:-1])
 
-    a = tf.reduce_sum(output * output, axis=axis)
-    b = tf.reduce_sum(target * target, axis=axis)
+    # define weights for each class
+    weights = 1.0 / ((tf.reduce_sum(target, axis=axis[:-1]) ** 2) + smooth)
 
-    union = a + b - intersection
-    score = (intersection + smooth) / (union + smooth)
-    score = tf.reduce_mean(score, name=_name)
+    # numerator and denominator:
+    numerator = tf.reduce_sum(weights * intersection, axis=1)
+    denominator = tf.reduce_sum(weights * (a + b), axis=1)
+
+    score = 2. * (numerator + smooth) / (denominator + smooth)
+    score = tf.reduce_mean(score, name='dice_coe')
     return score
-
-
-def iou_coe(output, target, axis=(1, 2, 3), smooth=1e-12):
-    """Wrapper to Jaccard (also known as Intersection over Union) coefficient """
-    return jaccard_coe(output, target, axis, smooth, _name='iou_coe')
