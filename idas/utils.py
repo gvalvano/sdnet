f8f9f17203ef694271a3e4f5cbb13783c9964e12
gabriele <gabriele.valvano@gmail.com>
2019-08-16 10:26:46 +0100

    fix documentation and remove unused functions

--- idas/utils.py
+++ idas/utils.py
@@ -1,27 +1,31 @@
 #  Copyright 2019 Gabriele Valvano
-# 
+#
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
 #  You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import os
 from tensorflow.python.client import device_lib
+import time
+import sys
+import os
 
 
 def get_available_gpus():
+    """ get list of available GPUs on the machine. """
     local_device_protos = device_lib.list_local_devices()
     return [x.name for x in local_device_protos if x.device_type == 'GPU']
 
 
 def get_available_cpus():
+    """ get list of available CPUs on the machine. """
     local_device_protos = device_lib.list_local_devices()
     return [x.name for x in local_device_protos if x.device_type == 'CPU']
 
@@ -35,7 +39,7 @@ def safe_mkdir(path):
 
 
 def print_yellow_text(text, sep=True):
-    """ useful for debug """
+    """ Print a yellow text. Useful for debugging. """
     if sep:
         print('_' * 40)  # line separator
     print('\033[1;33m{0}\033[0m'.format(text))
